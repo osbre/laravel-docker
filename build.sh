c8491b1c19b9bc32136d2d88211af1d7222c0706
Ostap Brehin <osbre@protonmail.com>
2024-01-11 19:21:37 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,32 +6,28 @@ set -e
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 phpVersions=(
-    7.4
-    8.0
-    8.1
-    8.2
     8.3
 )
 
 for phpVersion in ${phpVersions[@]}; do
     # Build Nginx + PHP-FPM flavors
-    docker build \
+    docker buildx build --platform=linux/arm64  \
       --build-arg PHP_VERSION="${phpVersion}" \
-      -t "fideloper/fly-laravel:${phpVersion}" \
+      -t "osbre/laravel:${phpVersion}" \
       -f "${SCRIPT_DIR}/src/Dockerfile" \
       "${SCRIPT_DIR}/src"
 
-    # Build Nginx Unit flavors
-    docker build \
-      --build-arg PHP_VERSION="${phpVersion}" \
-      -t "fideloper/fly-laravel:unit-${phpVersion}" \
-      -f "${SCRIPT_DIR}/src/Dockerfile-unit" \
-      "${SCRIPT_DIR}/src"
+    # # Build Nginx Unit flavors
+    # docker build \
+    #   --build-arg PHP_VERSION="${phpVersion}" \
+    #   -t "osbre/laravel:unit-${phpVersion}" \
+    #   -f "${SCRIPT_DIR}/src/Dockerfile-unit" \
+    #   "${SCRIPT_DIR}/src"
 done
 
 if [[ $# -gt 0 ]] && [[ $1 == "push" ]]; then
     for phpVersion in ${phpVersions[@]}; do
-        docker push "fideloper/fly-laravel:${phpVersion}"
-        docker push "fideloper/fly-laravel:unit-${phpVersion}"
+        docker push "osbre/laravel:${phpVersion}"
+        # docker push "osbre/laravel:unit-${phpVersion}"
     done
 fi
